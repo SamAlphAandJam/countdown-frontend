b1fad47d08b37ba73b9690ebddb1140f3aae8ff5
Tobe0504 <ezimorahtobenna@gmail.com>
2024-12-17 15:00:32 +0100

    added the new date and year text

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function countdownToLaunch(targetDate) {
   return { days: days, hours: hours, minutes, minutes, seconds: seconds };
 }
 
-const launchDate = new Date("2024-01-01T00:00:00").getTime();
+const launchDate = new Date("2025-01-01T00:00:00").getTime();
 const countdown = countdownToLaunch(launchDate);
 
 const daysDiv = document.querySelector(".countdown > div:nth-child(1) > h2");
